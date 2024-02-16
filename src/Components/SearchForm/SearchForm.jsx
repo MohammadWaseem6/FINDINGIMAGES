2bb246c13ce62e913140be292e08aa500552eb6e
WASEEM AHMAD MEER <120891700+MohammadWaseem6@users.noreply.github.com>
2024-02-16 18:20:40 +0530

    try

--- src/Components/SearchForm/SearchForm.jsx
+++ src/Components/SearchForm/SearchForm.jsx
@@ -17,7 +17,7 @@ const SearchForm = ({ onSubmit }) => {
   placeholder="Search for images..."
   className="border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
 />
-
+x 
      <button type="submit" className="border-2 border-gray-400 px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-600 transition-colors duration-300 ease-in-out">
   Search
 </button>
